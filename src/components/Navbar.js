4f447e6d3c4e738c25550d792c7008a0cbb7a817
bushido <nagpranav007@gmail.com>
2022-08-18 17:06:23 +0530

    final

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<img src="./images/react-icon-small.png" alt="react-logo" />
+			<h2>ReactFacts</h2>
+			<h3>React Course-Project 1</h3>
+		</nav>
+	);
+}
