297f22c8413976ef7512059cf0c36da516da3ce9
bushido <nagpranav007@gmail.com>
2022-08-18 17:38:22 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import reactLogo from "../images/react-icon-small.png";
 
 export default function Navbar() {
 	return (
 		<nav className="navbar">
-			<img src="./images/react-icon-small.png" alt="react-logo" />
+			<img src={reactLogo} alt="react-logo" />
 			<h2>ReactFacts</h2>
 			<h3>React Course-Project 1</h3>
 		</nav>
